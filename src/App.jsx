f2bfb009c03091d6dff0fe3a3c40c3595a02d926
Balla-Deepthi <deepthib9133@gmail.com>
2025-05-21 16:12:32 +0530

    setup react-router-dom with basic dashboard route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { Button } from "@/components/ui/button"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Sidebar from "./components/sidebar/Sidebar"; // update path as needed
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
-    </div>
-  )
+    <BrowserRouter>
+      <div className="flex">
+        <Sidebar />
+        <div className="flex-1 p-4">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            {/* Add more routes if needed */}
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
